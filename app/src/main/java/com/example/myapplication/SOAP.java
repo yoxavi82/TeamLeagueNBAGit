b4f281d1559ef2766cd197f9ce0ef2b7efb028b1
albertotiffon <albertotiffon@gmail.com>
2019-05-15 20:10:34 +0200

    Registrar sin calendario

--- app/src/main/java/com/example/myapplication/SOAP.java
+++ app/src/main/java/com/example/myapplication/SOAP.java
@@ -1,22 +1,22 @@
 package com.example.myapplication;
 
 
-import org.ksoap2.SoapEnvelope;
-import org.ksoap2.serialization.SoapObject;
-import org.ksoap2.serialization.SoapSerializationEnvelope;
-
-public class SOAP {
-    String NAMESPACE = "http://remotemysql.com/";
-    String URL="";
-    String METHOD_NAME = "";
-    String SOAP_ACTION = "";
-
-    SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
-
-    public SoapObject getRequest() {
-        return request;
-    }
-
-    SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-
-}
+//import org.ksoap2.serialization.SoapObject;
+//import org.ksoap2.SoapEnvelope;
+//import org.ksoap2.serialization.SoapSerializationEnvelope;
+//
+//public class SOAP {
+//    String NAMESPACE = "http://remotemysql.com/";
+//    String URL="";
+//    String METHOD_NAME = "";
+//    String SOAP_ACTION = "";
+//
+//    SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+//
+//    public SoapObject getRequest() {
+//        return request;
+//    }
+//
+//    SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+//
+//}
