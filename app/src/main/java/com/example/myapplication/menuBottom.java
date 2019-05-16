03b341da94944cfc3064cf14c17e2bbc3cd6331f
albertotiffon <albertotiffon@gmail.com>
2019-05-16 22:40:13 +0200

    Mensaje de inicio

--- app/src/main/java/com/example/myapplication/menuBottom.java
+++ app/src/main/java/com/example/myapplication/menuBottom.java
@@ -5,11 +5,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class menuBottom extends AppCompatActivity {
 
     private TextView mTextMessage;
+    Button crear,unir;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -36,9 +39,21 @@ public class menuBottom extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_bottom);
 
+        crear = findViewById(R.id.crear);
+        unir = findViewById(R.id.unirse);
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void unirseMetod(View view){
+        System.out.println("unir");
+
+    }
+
+    public  void crearMetod(View view){
+        System.out.println("crear");
+
     }
 
 }
