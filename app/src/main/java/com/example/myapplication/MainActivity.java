2c08167805282e75fa1d17f7ed59f233262faf92
albertotiffon <albertotiffon@gmail.com>
2019-05-22 19:39:33 +0200

    Optimizado el menu

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,8 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.File;
-
 public class MainActivity extends AppCompatActivity {
     Button entrar,resgitrar;
 
--- app/src/main/java/com/example/myapplication/RegisterView.java
+++ app/src/main/java/com/example/myapplication/RegisterView.java
@@ -1,24 +1,17 @@
 package com.example.myapplication;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplication.pojos.Usuarios;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -28,7 +21,6 @@ public class RegisterView extends AppCompatActivity {
     TextView usuario,nombre,apellidos,correo;
     String fechaDia,fechaMes,fechaAño;
     String fechaNacimiento;
-//    CalendarView calendario;
     Spinner dia,mes,año;
     Button accionRegistrar;
     ArrayList<String> usuarios =new ArrayList<>();
@@ -49,9 +41,7 @@ public class RegisterView extends AppCompatActivity {
         dia = findViewById(R.id.dia);
         mes = findViewById(R.id.mes);
         año = findViewById(R.id.año);
-//        calendario = findViewById(R.id.Calendario);
         accionRegistrar = findViewById( R.id.registrar);
-//        sqlThread.start();
 
         for(int i =1 ;i<=31;i++){
             dias.add(i+"");
@@ -95,65 +85,8 @@ public class RegisterView extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
-
-
-
-
-
-//        calendario.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-//            @Override
-//            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-//                fecha = dayOfMonth+"/"+month+"/"+year;
-//            }
-//        });
-
-//        usuario.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-//            @Override
-//            public void onFocusChange(View v, boolean hasFocus) {
-//                if(!hasFocus){
-//                    for(int i =0;i<usuarios.size();i++){
-//                        if(usuarios.get(i)==usuario.getText().toString()){
-//                            existe=true;
-//                        }
-//                    }
-//                }
-//            }
-//        });
     }
 
-//    public Connection connection(){
-//        Connection conexion = null;
-//        try{
-//
-//
-//        }catch (Exception e){
-//
-//        }
-//        return  conexion;
-//    }
-//
-//
-//    Thread sqlThread = new Thread() {
-//        public void run() {
-//            try {
-//                Class.forName("com.mysql.jdbc.Driver");
-//                Connection conn = DriverManager.getConnection(
-//                        "jdbc:mysql://remotemysql.com/k0zCh3gTgb", "k0zCh3gTgb", "HD5V4w6oyv");
-//                String stsql = "Select Id_Usuario from Usuarios";
-//                Statement st = conn.createStatement();
-//                ResultSet rs = st.executeQuery(stsql);
-//                while (rs.next()){
-//                    usuarios.add(rs.getString(0));
-//                    System.out.println( rs.getString(0) );
-//                }
-//                conn.close();
-//            } catch (SQLException se) {
-//                System.out.println("oops! No se puede conectar. Error: " + se.toString());
-//            } catch (ClassNotFoundException e) {
-//                System.out.println("oops! No se encuentra la clase. Error: " + e.getMessage());
-//            }
-//        }
-//    };
 
     public  void crearFecha(){
         if(fechaAño!=null&&fechaDia!=null&&fechaMes!=null){
