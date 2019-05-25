e0af34a14da58106656378171a8e1dc0d71f956c
yoxavi82 <47785054+yoxavi82@users.noreply.github.com>
2019-05-26 00:55:00 +0200

    Clase Conexion OK

---
+++ app/src/main/java/com/example/teamleaguebagit/Conexion.java
@@ -0,0 +1,35 @@
+package com.example.teamleaguebagit;
+
+import android.os.StrictMode;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    static String url = "jdbc:mysql://remotemysql.com:3306/k0zCh3gTgb";
+    static String user = "k0zCh3gTgb";
+    static String passwrd = "HD5V4w6oyv";
+
+    public static Connection obtenerConexion(){
+
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url, user, passwrd);
+            if (connection != null) {
+                return connection;
+            }
+            return null;
+        }catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
